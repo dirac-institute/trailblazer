deda2273d697245820643dc6c7fefd939ca3b1da
Meredith Rawls <mrawls@uw.edu>
2021-06-29 13:38:57 -0700

    Mess with settings for local deployment only

--- trail/trail/settings.py
+++ trail/trail/settings.py
@@ -13,10 +13,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-from .config import DbAuth, SiteConfig
+# from .config import DbAuth, SiteConfig
+from .config import SiteConfig
 
-siteConfig = SiteConfig.fromYaml()
-dbConfig = DbAuth.fromYaml()
+# siteConfig = SiteConfig.fromYaml()
+siteConfig = SiteConfig({"settings": {"secret_key": "alalal"}}, False, "")
+# dbConfig = DbAuth.fromYaml()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
