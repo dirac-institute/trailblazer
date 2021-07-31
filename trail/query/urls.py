575ac750279737976e951481c90e54e4d5511916
DinoBektesevic <dinob@uw.edu>
2021-07-31 12:47:45 +0200

    Add an example of how ModelForms work.
    Had not yet had the time to install the 3rd party app
    "django_tables2" and give it a good shake. The table class would
    still live in the views.py and would give yet another way to map
    our models to a table in this case, so I removed all that I
    percieved as extraneous code.
    
    Added verbose comments.
    
    In short:
    
    1) visiting home page and clicking query on the navigation
    takes us to the root url of our query app. This does not by default
    render any tables.
    2) Submitting a query further redirects us to a query/results url
    which by default always renders a table, with or without results.
    
    How:
    The ModelForm is used to map fields of our DB schema models onto
    entry, text or date inut boxes.
    The model form is rendered by the templating engine. That python
    code becomes HTML.
    We add an additional button to the form in the HTML. That button
    activates an action, redirecting us to query/results url, by sending
    a POST type request to our server.
    We instantiate our model form again from the POST request and
    validate input.
    We access the posted data of our form, currently just the instrument,
    and execute a query. We should probaby call clean data and access the
    cleaned data. We should also probably define what clean method does
    on our form (such as blocks all escape characters, converts everything
    to lowercase letters etc..).
    We return the queried results back to our templating engine, which
    then renders them as HTML table.
    In the case we somehow find ourselfs invoking print_results (by
    visiting query/results URL) without making a POST request we render
    the table header with no results and the form with no input.
    We can rely on the fact we marked the instrument field as a required
    field to skip checking whether there is any content in it. For example,
    if you wanted to do the same for telescope, you would have to verify
    the user did not just leave the box empty, and then issue an appropriate
    query, one that would select * from...

--- trail/query/urls.py
+++ trail/query/urls.py
@@ -1,7 +1,5 @@
 from query.views import print_results, index
 from django.urls import path
-from query.views import ObservationListView
-# from django.views.generic import TemplateView
 
 
 """Route query URLs to query views here."""
@@ -10,5 +8,4 @@ from query.views import ObservationListView
 urlpatterns = [
    path('', index, name='query'),
    path('results', print_results, name='results'),
-   path("observation", ObservationListView.as_view())
 ]
--- trail/query/views.py
+++ trail/query/views.py
@@ -1,48 +1,94 @@
-from django.apps import apps
-# from django.http import HttpResponse
-from django.shortcuts import render
+from django import forms
 from django.forms import ModelForm
-from django.forms import BooleanField
-from query.forms import Settings
-from django_tables2 import SingleTableView
-from .models import Observation
-from .tables import ObservationTable
-
-class ObservationListView(SingleTableView):
-    model = Observation
-    table_class = ObservationTable
-    template_name = "query/observation.html"
-
-class SettingsForm(ModelForm):
-    receive_newsletter = BooleanField()
-
-
-class Meta:
-    model = Settings
-
-
-# not used
-def index(request):
-    # return HttpResponse("Wouldn't it be nice if someone wrote a query tool here?")
-    return render(request, "query.html")
+from django.apps import apps
+from django.shortcuts import render
 
 
 Metadata = apps.get_model('upload', 'Metadata')
 
-# This loads how the metadata database is structured
-# Metadata = apps.get_model('upload', Metadata)
+# This is the ModelForm on the link I sent you. We import the Metadata DB model
+# fom Uploads, we then map the felds from that model onto our Form. We can,
+# during that mapping, mark some fields as mandatory and some as not. We can
+# give some constraints on our inputs, such as length or format. We can pick
+# from a variety of different input fields, from charaters, to number, dates,
+# to long text form input. Some examples of mandatory/nonmandatory, validation
+# and type of inputs provided.
+class MetadataForm(ModelForm):
+    instrument = forms.CharField(max_length=20)
+    telescope = forms.CharField(max_length=20, required=False)
+
+    # processor_name = forms.CharField(max_length=20, required=False)
+    # standardizer_name = forms.CharField(max_length=20, required=False)
+    # datetime_begin = forms.DateTimeField(
+    #     input_formats=['%d/%m/%Y %H:%M:%S'],
+    #     widget=forms.DateTimeInput(attrs={
+    #         'class': 'form-control datetimepicker-input',
+    #         'data-target': '#datetimepicker_begin'
+    #     }),
+    #     required=False
+    # )
+    # datetime_end = forms.DateTimeField(
+    #     input_formats=['%d/%m/%Y %H:%M:%S'],
+    #     widget=forms.DateTimeInput(attrs={
+    #         'class': 'form-control datetimepicker-input',
+    #         'data-target': '#datetimepicker_end'
+    #     }),
+    #     required=False
+    # )
+
+    # This maps our database schema model columns onto our form entries
+    class Meta:
+        model = Metadata
+        fields = ['instrument', "telescope", ]
+
+
+def index(request):
+    """Index renders the form when root url is visited, bypassing slower checks
+    required fo rendering of the results url, where rendering of the table and
+    checking for results are performed.
+
+    It is assumed the request type is POST.
+    """
+    form = MetadataForm()
+    return render(request, "query.html", {"queryform": form, "render_table":False})
 
 
 def print_results(request):
-    """This function runs takes a request and renders some html on the query.html page."""
-    # This is the django way of doing `select * from upload_metadata;
-    emptylist = []
+    """Renders the results url, which is a placeolder copy of the root url of
+    query interface, where any results are rendered alongside the table headers.
+    """
     if request.method == "POST":
-        userdata = request.POST.get("Inst")
-        query_result = Metadata.objects.filter(instrument__icontains=userdata)
-  
-        for item in query_result:
-            itemDict = item.toDict()
-            emptylist.append(itemDict)
-    # breakpoint()
-    return render(request, "query.html", {"data": emptylist})
+        # when the request is type post (user sending data to us), then we want
+        # to instantiate the form using the post request, and leave ourselfs the
+        # option of using that data in our form as well.
+        form = MetadataForm(request.POST)
+
+        # triggers validation of input data, for now we don't have any real
+        # limits on what kinds of strings users can write in our form, except
+        # that most of them can't be longer than 20 characters and that datetime
+        # has to be in the '%d/%m/%Y %H:%M:%S' format. It's good to leave us
+        # with the ability to implement any changes in the future more easily
+        # so we perform validation now, before making the query and waste CPU
+        if form.is_valid():
+            # This is the django way of doing `select * from upload_metadata;
+            query_results = Metadata.objects.filter(instrument__icontains=form.data["instrument"])
+
+            # Previously, so that you can see the results when you're
+            # debugging, we converted each one to a dictionary. Dicts have
+            # pretty print outputs by default. I will add a nice strigification
+            # to our models when I manage, and then they wll also have pretty
+            # outputs too. Until then they wont. Nevertheless it's not
+            # *strictly neccessary* to convert results to dictionaries. They
+            # already behave like a list of dicts, they just have ugly default
+            # printing.
+            #for item in query_result:
+            #    itemDict = item.toDict()
+            #    emptylist.append(itemDict)
+    else:
+        # if *somehow* we ended in results url, but the method type is not POST
+        # (no data was sent), we render an empty form again and send back an
+        # empty list of results
+        query_results = []
+        form = MetadataForm()
+
+    return render(request, "query.html", {"data": query_results, "queryform": form, "render_table":True})
