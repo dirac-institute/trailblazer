ba6cbe6fdb45063e511918f95cee2481e4a21f01
JediJoe100 <thisisjedijoe@gmail.com>
2021-08-28 10:18:50 +1200

    For gallery.html cleaned up the article layout
    For views.py pulled information from the database and sorted it by dates before sending it to the webpage. For now just using one of the static images in media for every single entry since the metadata doesn't reference any images yet.

--- trail/gallery/views.py
+++ trail/gallery/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.apps import apps
 import os
 
+Metadata = apps.get_model('upload', 'Metadata')
 """This code runs when a user visits the 'gallery' URL."""
+
+
+def date_sort(e):
+    return e["datetime_begin"]
 
 
 def get_images(count):
@@ -13,10 +19,26 @@ def get_images(count):
     except FileNotFoundError:
         return []
     else:
-        for i, imageName in enumerate(imageNames):
-            images.append({"name": imageName,
-                           "path": os.path.join(path, imageName),
-                           "caption": imageName})
+        # Would there maybe a way to sort the data without converting to a list?
+        # converting to a list seems computationally heavy
+
+        #getting the data from database
+        data = list(Metadata.objects.values())
+
+        #sorting data by date
+        data.sort(reverse=True, key=date_sort)
+
+        #processing data for sending to user
+        for image in data:
+            images.append({"name": imageNames[0],
+                           "path": os.path.join(path, imageNames[0]),
+                           "caption": image["telescope"],
+                           "date": image["datetime_begin"]})
+        # for i, imageName in enumerate(imageNames):
+        #     images.append({"name": imageName,
+        #                    "path": os.path.join(path, imageName),
+        #                    "caption": imageName,
+        #                    "date": "undefined"})
         return images
 
 
