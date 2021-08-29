e414bcaf6c01c83e98fd23ad434d44ea4c495eaa
JediJoe100 <thisisjedijoe@gmail.com>
2021-08-30 10:43:27 +1200

    Added pulling image location from the database (rather crudely though)

--- trail/gallery/views.py
+++ trail/gallery/views.py
@@ -5,12 +5,14 @@ import os
 
 Metadata = apps.get_model('upload', 'Metadata')
 Thumbnails = apps.get_model('upload', 'Thumbnails')
+Wcs = apps.get_model('upload', 'Wcs')
+
 
 """This code runs when a user visits the 'gallery' URL."""
 
 
 def date_sort(e):
-    return e["datetime_begin"]
+    return e["date"]
 
 
 def get_images(count):
@@ -23,21 +25,22 @@ def get_images(count):
     else:
         # Would there maybe a way to sort the data without converting to a list?
         # converting to a list seems computationally heavy
-        print(Thumbnails.objects.values())
         # getting the data from database
-        data = list(Metadata.objects.values())
+        image_data = list(Thumbnails.objects.values())
 
         # sorting data by date
-        data.sort(reverse=True, key=date_sort)
-        print(data)
 
         # processing data for sending to user
-        for image in data:
-            images.append({"name": imageNames[0],  # at the moment this is just pulling the static files
-                           "id": image["id"],
-                           "path": os.path.join(path, imageNames[0]),
-                           "caption": image["telescope"],
-                           "date": image["datetime_begin"]})
+        for image in image_data:
+            wcs_data = Wcs.objects.values()[image["wcs_id"]-1]
+            metadata = Metadata.objects.values()[wcs_data["metadata_id"]-1]
+            images.append({"name": image["small"],  # at the moment this is just pulling the static files
+                           "id": image["wcs_id"],
+                           "path": os.path.join(path, image["small"]),
+                           "caption": metadata["telescope"],
+                           "date": metadata["datetime_begin"]})
+
+        images.sort(reverse=True,key=date_sort)
         # for i, imageName in enumerate(imageNames):
         #     images.append({"name": imageName,
         #                    "path": os.path.join(path, imageName),
@@ -51,5 +54,10 @@ def render_gallery(request, count=20):
     return render(request, "gallery.html", {'data': images})
 
 def render_image(request):
-    image_data = Metadata.objects.filter(id=int(request.get_full_path_info().split("?")[1]))[0]
-    return render(request, "images.html", {'image_data': image_data})
+    id = int(request.get_full_path_info().split("?")[1])
+    image_data = Thumbnails.objects.filter(wcs_id=id)[0]
+    wcs_data = Wcs.objects.values()[id-1]
+    metadata = Metadata.objects.values()[wcs_data["metadata_id"]-1]
+
+    print()
+    return render(request, "images.html", {'image_data': metadata, 'image': image_data})
