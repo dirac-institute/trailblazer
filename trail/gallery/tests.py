12ab76f18eafa9c4087b527bdf7ff23e08381535
JediJoe100 <thisisjedijoe@gmail.com>
2021-09-10 15:29:09 +1200

    White space clean up

--- trail/gallery/tests.py
+++ trail/gallery/tests.py
@@ -2,11 +2,12 @@ from django.test import TestCase
 from gallery.views import get_image
 from uploads.models import Thumbnails
 
+
 class GalleryImageTestCase(TestCase):
     def setUp(self):
         objlist = []
         for i in range(100):
-            objlist.append(Thumbnails({"wcs-id": i, "small": "", "large":""}))
+            objlist.append(Thumbnails({"wcs-id": i, "small": "", "large": ""}))
         Thumbnails.objects.create(objlist)
 
     def testImageListSame(self):
@@ -14,6 +15,5 @@ class GalleryImageTestCase(TestCase):
         for count in range(20):
             page = 0
             processed = [image.wcs_id for image in get_image(count, page)]
-            expected = list(range(page*count, (page+1)*count))
+            expected = list(range(page * count, (page + 1) * count))
             self.assertTrue(processed == expected)
-
