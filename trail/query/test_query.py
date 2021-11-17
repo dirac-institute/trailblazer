4f3e48849137f2954ce37e1d15e593489d39644c
Jasmine Watt <enteruws@uw.edu>
2021-11-17 09:16:01 -0800

    In the progress of fixing partial match. Added case-sensitive funciton.

--- trail/query/test_query.py
+++ trail/query/test_query.py
@@ -4,7 +4,7 @@ Test3 = {'instrument': 'DECam', 'telescope': 'asd', 'processor_name': 'asss', 'c
 Test4 = {'instrument': 'DECam', 'telescope': ' ', 'processor_name': 'asss', 'col': ' '}
 
 
-def get_query1(test):
+def get_query1(test, arugment=True):
     # values = test.copy()
     # breakpoint()
     # values.pop("csrfmiddlewaretoken", False)
@@ -13,43 +13,43 @@ def get_query1(test):
     new_dict = {}
     for key in test:
         if test[key] != ' ':
-            # add the key 
-            # test.pop(key)
-            new_dict[key] = test[key]
+            if arugment:
+                keyk = key + "__contains"
+                new_dict[keyk] = test[key]
     return new_dict
 
 
-def get_query(d, argument = True):
-    new_dict = {}
-    # argument = True
-    for key in d:
-        if d[key]:
-            # add the key 
-            # test.pop(key)
-            if argument == True:
-                new_dict[key] = d[key]
-    return new_dict
+# def get_query(d, argument=True):
+#     new_dict = {}
+#     # argument = True
+#     for key in d:
+#         if d[key]:
+#             if argument:
+#                 keyk = key + "__contains"
+#                 new_dict[keyk] = d[key]
+#     return new_dict
 
-print(get_query(Test1, False))
+
+print(get_query1(Test1))
 # print(get_query(Test2))
-#print(get_query(Test3))
-#print(get_query(Test4))
+# print(get_query(Test3))
+# print(get_query(Test4))
 
 
-class TestClass():
-    b = 1
-    def __init__(self, a):
-        self.aa = a
-    def pprinter(self):
-        print(self.aa, self.b)
+# class TestClass():
+#     b = 1
+#     def __init__(self, a):
+#         self.aa = a
+#     def pprinter(self):
+#         print(self.aa, self.b)
 
-tc = TestClass({"a":'1', "b":'2'})
-tc1 = TestClass('e')
-print(tc.aa)
-print(tc.b)
-print(TestClass.b)
-# print(TestClass.a)
-TestClass.b = 2
-print(tc.b)
-print(tc1.aa)
-print(tc.pprinter())
+# tc = TestClass({"a":'1', "b":'2'})
+# tc1 = TestClass('e')
+# print(tc.aa)
+# print(tc.b)
+# print(TestClass.b)
+# # print(TestClass.a)
+# TestClass.b = 2
+# print(tc.b)
+# print(tc1.aa)
+# print(tc.pprinter())
