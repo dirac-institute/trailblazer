57eafd1e8c42eea1069e6608bb10c0817c5f1045
arihani2 <arihani2@illinois.edu>
2022-04-21 14:23:01 -0500

    Standardizer is almost done. Waiting for confirmation on instrument and telescope names

--- trail/upload/process_uploads/standardizers/gmosn_standardizer.py
+++ trail/upload/process_uploads/standardizers/gmosn_standardizer.py
@@ -2,7 +2,7 @@
 Class that facilitates header metadata translation for Gemini North instrument
 """
 
-from datetime import timezone
+from datetime import datetime, timedelta
 
 from astropy.time import Time
 
@@ -24,56 +24,43 @@ class GmosnStandardizer(HeaderStandardizer):
     @classmethod
     def canStandardize(self, header, **kwargs):
         lat = header.get("GEOLAT", False)
-        # obstel = header.get("OBSTEL", False)
-        if lat and 19.8200 == lat:
-            return True
+        if lat and 19.8200 == lat: return True
         return False
 
-    # def standardizeMetadata(self):
-    #     run = self.header["RUN"].strip()
-    #     field = self.header["FIELD"].strip()
-    #     filter = self.header["COLOUR"].strip()
-    #     chip = self.header["CHIP"]
-    #     sciProg = f"{run}-{field}-{filter}-{chip}"
+    def standardizeMetadata(self):
 
-    #     # TODO: Fix datetimes
-    #     # There is a timesys key but I have no idea how to generically instantiate
-    #     # timezone aware datetime and astropy Time seems not to work well with
-    #     # Django - the astrometadata is also broken!
-    #     if "UTC" in self.header["TIMESYS"].upper():
-    #         tzinfo = timezone.utc
-    #     else:
-    #         raise ValueError("Can not recognize time scale system that is used?")
+        # TODO: Fix datetimes
+        # There is a timesys key but I have no idea how to generically instantiate
+        # timezone aware datetime and astropy Time seems not to work well with
+        # Django - the astrometadata is also broken!
+        DATEOBS = self.header["DATE-OBS"]
+        EXP = self.header["EXPTIME"]
+        dec = DATEOBS.find('.')
+        if dec != -1: date = DATEOBS[:dec]
+        begin = datetime.fromisoformat(date)
+        end = begin + timedelta(seconds= EXP)
 
-    #     jdstart = Time(self.header["JDSTART"], format="jd", scale="utc")
-    #     jdstart = jdstart.utc.datetime
-    #     jdstart = jdstart.replace(tzinfo=tzinfo)
+        # TODO: filter out what is the filter standardization here?
+        # After uploading, no "astrometry.net key" runtime err?
+        meta = Metadata(
+            obs_lon=self.header["GEOLON"],
+            obs_lat=self.header["GEOLAT"],
+            obs_height= 4213, #height in meters from official website
+            datetime_begin=begin,
+            datetime_end= end,
+            telescope="Gemini North",
+            instrument="GMOS", 
+            exposure_duration=self.header["EXPTIME"],
+            filter_name=self.header["FILTER"].strip()
+        )
 
-    #     jdend = Time(self.header["JDSTART"], format="jd", scale="utc")
-    #     jdend = jdend.utc.datetime
-    #     jdend = jdend.replace(tzinfo=tzinfo)
+        return meta
 
-    #     # TODO: filter out what is the filter standardization here?
-    #     meta = Metadata(
-    #         obs_lon=self.header["LOGITUD"],
-    #         obs_lat=self.header["LATITUD"],
-    #         obs_height=self.header["HEIGHT"],
-    #         datetime_begin=jdstart.isoformat(),
-    #         datetime_end=jdend.isoformat(),
-    #         telescope=self.header["OBSTEL"].strip(),
-    #         instrument=self.header["CAMERA"].strip(),
-    #         science_program=sciProg,
-    #         exposure_duration=self.header["EXPTIME"],
-    #         filter_name=self.header["COLOUR"].strip()
-    #     )
-
-    #     return meta
-
-    # def standardizeWcs(self, **kwargs):
-    #     # ignores any WCS information in the header and instead just sends it to astrometry.net to solve.
-    #     # need to add a catch that catches the errors in astrometryNetSolver
-    #     try:
-    #         header, dimX, dimY = self._astrometryNetSolver(self._kwargs['filepath'])
-    #     except ValueError and RuntimeError and TypeError as err:
-    #         raise StandardizeWcsException("Failed to standardize WCS") from err
-    #     return Wcs(**self._computeStandardizedWcs(header, dimX, dimY))
+    def standardizeWcs(self, **kwargs):
+        # ignores any WCS information in the header and instead just sends it to astrometry.net to solve.
+        # need to add a catch that catches the errors in astrometryNetSolver
+        try:
+            header, dimX, dimY = self._astrometryNetSolver(self._kwargs['filepath'])
+        except ValueError and RuntimeError and TypeError as err:
+            raise StandardizeWcsException("Failed to standardize WCS") from err
+        return Wcs(**self._computeStandardizedWcs(header, dimX, dimY))
