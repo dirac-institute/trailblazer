cc05fe86c958d244ac7e8e6a37fd91499c4a3e00
arihani2 <arihani2@illinois.edu>
2022-04-28 12:19:50 -0500

    Fixed all flake8 errors and should be ready for a merge

--- trail/upload/process_uploads/standardizers/gmosn_standardizer.py
+++ trail/upload/process_uploads/standardizers/gmosn_standardizer.py
@@ -4,10 +4,8 @@ Class that facilitates header metadata translation for Gemini North instrument
 
 from datetime import datetime, timedelta, timezone
 
-from astropy.time import Time
-
-from upload.process_uploads.header_standardizer import HeaderStandardizer, StandardizeWcsException
-from upload.models import Metadata, Wcs
+from upload.process_uploads.header_standardizer import HeaderStandardizer
+from upload.models import Metadata
 
 
 __all__ = ["GmosnStandardizer", ]
@@ -24,41 +22,44 @@ class GmosnStandardizer(HeaderStandardizer):
     @classmethod
     def canStandardize(self, header, **kwargs):
         lat = header.get("GEOLAT", False)
-        if lat and 19.8200 == lat: return True
+        if lat and 19.8200 == lat:
+            return True
         return False
 
     def standardizeMetadata(self):
         DATEOBS = self.header["DATE-OBS"]
         EXP = self.header["EXPTIME"]
         begin = datetime.strptime(DATEOBS, "%Y-%m-%dT%H:%M:%S.%f")
-        begin = begin.replace(tzinfo = timezone.utc)
-        end = begin + timedelta(seconds= EXP)
+        begin = begin.replace(tzinfo=timezone.utc)
+        end = begin + timedelta(seconds=EXP)
 
-        # Need confirmation on what october date. No clear day, but says mid october
-        # Also not sure what day in June, assumed first day for now
+        # Need confirmation on what october date. No clear day,
+        # but says mid october. Also not sure what day in June,
+        # assumed first day for now
         oct_string = "2011-10-01T00:00:00.0"
         october11 = datetime.strptime(oct_string, "%Y-%m-%dT%H:%M:%S.%f")
-        october11 = october11.replace(tzinfo = timezone.utc)
+        october11 = october11.replace(tzinfo=timezone.utc)
         june_string = "2014-06-01T00:00:00.0"
         june14 = datetime.strptime(june_string, "%Y-%m-%dT%H:%M:%S.%f")
         june14 = june14.replace(tzinfo=timezone.utc)
 
-
-        if begin < october11: instrument = "EEV"
-        elif begin < june14: instrument = "e2v DD"
-        else: instrument = "Hamamatsu"
-
+        if begin < october11:
+            instrument = "EEV"
+        elif begin < june14:
+            instrument = "e2v DD"
+        else:
+            instrument = "Hamamatsu"
 
         meta = Metadata(
             obs_lon=self.header["GEOLON"],
             obs_lat=self.header["GEOLAT"],
-            obs_height= 4213, #height in meters from official website
+            obs_height=4213,  # height in meters from official website
             datetime_begin=begin.isoformat(),
-            datetime_end= end.isoformat(),
+            datetime_end=end.isoformat(),
             telescope="Gemini North",
-            instrument= instrument,
+            instrument=instrument,
             exposure_duration=self.header["EXPTIME"],
             filter_name=self.header["FILTER"].strip()
         )
-        
+
         return meta
