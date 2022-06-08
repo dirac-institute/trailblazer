13c15ce1eed76710674f1690e3ef4205e087ed66
arihani2 <arihani2@illinois.edu>
2022-06-08 13:38:39 -0500

    Found filter using regex

--- trail/upload/process_uploads/standardizers/vatt_standardizer.py
+++ trail/upload/process_uploads/standardizers/vatt_standardizer.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta, timezone
 from upload.process_uploads.header_standardizer import HeaderStandardizer
 from upload.models import Metadata
 
+import re
+
 
 __all__ = ["VattStandardizer", ]
 
@@ -50,6 +52,8 @@ class VattStandardizer(HeaderStandardizer):
         metadata: 'upload.models.Metadata'
             The metadata
         """
+
+        #The date and time are seperated in the header, I concatenate them together here
         DATE = self.header["DATE-OBS"]
         TIME = self.header["TIME-OBS"]
         DATEOBS = DATE + 'T' + TIME
@@ -58,15 +62,27 @@ class VattStandardizer(HeaderStandardizer):
         begin = begin.replace(tzinfo=timezone.utc)
         end = begin + timedelta(seconds=EXP)
 
+        expr = re.compile(r'Filter(.*)')
+        comment = str(self.header['COMMENT']).split('\n')
+        matches = [expr.match(line).groups()[0] for line in comment]
+        if len(matches) == 1:
+            FILTER = matches[0]
+        else:
+            FILTER = None
+        
         meta = Metadata(
             obs_lon=-109.892107,#Longitude from google maps
             obs_lat=32.701328,#Latitude from google maps
             obs_height=self.header["ELEVAT"],
             datetime_begin=begin.isoformat(),
             datetime_end=end.isoformat(),
+            #Some of the instrument data has a weird format
+            #They begin with a =" and end with a ", the
+            #indices get rid of those extra characters
             telescope=self.header["TELESCOP"][3:-1].strip(),
             instrument=self.header["INSTRUME"][3:-1].strip(),
             exposure_duration=self.header["EXPTIME"],
+            filter = FILTER
         )
 
         return meta
